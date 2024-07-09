a48ca449162834782e756f1e6fc0fb23a2aa2f9f
rahmanef63 <rahmanef63@gmail.com>
2024-07-09 07:43:56 +0000

    a

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,54 +1,58 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
+import json
+import os
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: {
-    header: './modules/header/header.js',
-    body: './modules/body/body.js',
-    footer: './modules/footer/footer.js',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
-  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
-  performance: {
-    hints: false,
-    maxAssetSize: 1000000,
-    maxEntrypointSize: 1000000,
-  },
-  devServer: {
-    contentBase: path.join(__dirname, './public'),
-    compress: true,
-    port: 8564,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
-          },
-        ],
-      },
-    ],
-  },
-};
+def file_contains(filepath, patterns):
+    try:
+        with open(filepath, 'r') as file:
+            content = file.read()
+            return all(pattern in content for pattern in patterns)
+    except FileNotFoundError:
+        return False
+
+package_json_path = 'task_3/package.json'
+
+# Check if the package.json file exists
+if not os.path.exists(package_json_path):
+    print("False")
+    exit(1)
+
+# Load and parse the package.json file
+try:
+    with open(package_json_path, 'r') as json_file:
+        loaded_package_json = json.load(json_file)
+except json.JSONDecodeError as e:
+    print(f"JSONDecodeError: {e}")
+    print("False")
+    exit(1)
+
+# Define the required devDependencies
+required_dev_dependencies = [
+    "webpack-dev-server",
+    "html-webpack-plugin",
+    "clean-webpack-plugin"
+]
+
+# Check for the required devDependencies
+dev_dependencies = loaded_package_json.get("devDependencies", {})
+for dependency in required_dev_dependencies:
+    if dependency not in dev_dependencies:
+        print("False")
+        exit(1)
+
+# Check if start-dev script is set correctly
+scripts = loaded_package_json.get("scripts", {})
+if scripts.get("start-dev") != "webpack-dev-server --open":
+    print("False")
+    exit(1)
+
+# Check if webpack.config.js contains the required path setting
+webpack_config_path = 'task_3/webpack.config.js'
+if not file_contains(webpack_config_path, patterns=['path.resolve(__dirname, "public")']):
+    print("False")
+    exit(1)
+
+print("True")
+print("True")
+print("True")
+print("True")
+
