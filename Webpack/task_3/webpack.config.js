725ac19201a51c00efc13e35de6247996a6fa992
rahmanef63 <rahmanef63@gmail.com>
2024-07-09 07:54:37 +0000

    a

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,54 +1,33 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: 'development', // Ensure mode is set to 'development'
   entry: {
-    header: './modules/header/header.js',
-    body: './modules/body/body.js',
-    footer: './modules/footer/footer.js',
+    header: './modules/header/header.js', // Ensure entry points are correctly set
+    body: './modules/body/body.js', // Example entry point
+    footer: './modules/footer/footer.js' // Example entry point
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.js', // Ensure output filename is set correctly
+    path: path.resolve(__dirname, 'public') // Ensure path is set correctly
   },
+  devtool: 'inline-source-map', // Ensure devtool is set to 'inline-source-map'
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'), // Ensure contentBase is set correctly
+    port: 8564 // Ensure port is set correctly
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html' // Ensure template file is set correctly
+    }),
+    new CleanWebpackPlugin() // Ensure CleanWebpackPlugin is used
+  ],
   optimization: {
     splitChunks: {
-      chunks: 'all',
-    },
-  },
-  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
-  performance: {
-    hints: false,
-    maxAssetSize: 1000000,
-    maxEntrypointSize: 1000000,
-  },
-  devServer: {
-    contentBase: path.join(__dirname, './public'),
-    compress: true,
-    port: 8564,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
-          },
-        ],
-      },
-    ],
-  },
+      chunks: 'all' // Ensure chunks are set to 'all'
+    }
+  }
 };
+
